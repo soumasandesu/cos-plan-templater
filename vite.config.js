dad24814ba307ae14a41e66957e20cdfb8023a4b
Charles Poon <cypoon@shoalter.com>
2025-12-19 15:26:10 +0800

    FIX dart sass

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "reset-css";`,
+        additionalData: `@use "reset-css";`,
       },
     },
   },
